47fb25732a05b95e76ff747aa2ec336624f1d546
j-stax <ahnjames01@gmail.com>
2024-10-27 17:18:20 -0400

    copy_directory_contents.py

---
+++ src/copy_directory_contents.py
@@ -0,0 +1,51 @@
+import os
+
+def copy_directory_contents(src_dir, dst_dir):
+    # Remove existing contents in the destination directory
+    for filename in os.listdir(dst_dir):
+        file_path = os.path.join(dst_dir, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                delete_directory(file_path)     # Call to delete_directory
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+    
+    # Copy contents from the source directory
+    for filename in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, filename)
+        dst_path = os.path.join(dst_dir, filename)
+        try:
+            if os.path.isfile(src_path) or os.path.islink(src_path):
+                with open(src_path, 'rb') as src_file:
+                    with open(dst_path, 'wb') as dst_file:
+                        print(f'Logging file path of source: {src_path}')   # Log path being copied
+                        dst_file.write(src_file.read())
+            elif os.path.isdir(src_path):
+                os.mkdir(dst_path)
+                copy_directory_contents(src_path, dst_path)
+        except Exception as e:
+            print(f'Failed to copy {src_path}. Reason: {e}')
+
+
+# Recursively delete contents for a given directory
+def delete_directory(dir_path):
+    for filename in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                delete_directory(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+    os.rmdir(dir_path)
+
+
+if __name__ == '__main__':
+    src_dir = "../static"
+    dst_dir = "../test"
+    copy_directory_contents(src_dir, dst_dir)
+        
+    
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from textnode import *
+from copy_directory_contents import copy_directory_contents
 
 def main():
-    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(text_node)
+    src_dir = './static'
+    dst_dir = './public'
+    copy_directory_contents(src_dir, dst_dir)
 
 main()
